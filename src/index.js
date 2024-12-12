9222d5178b5212a8dcf81b11b8a249f0395a0c5d
Manuel Araujo <manuelmad1981@gmail.com>
2024-12-12 18:49:04 -0400

    Adding the mssingMagazines function to main page (meican).

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import inventory from './database.js';
 
+import missingMagazines from '../functions/missingMagazines.js';
 import searchedMagazines from '../functions/searchedMagazines.js';
 import poorConditionMagazines from '../functions/poorConditionMagazines.js';
 
@@ -234,65 +235,10 @@ total_count.innerHTML =  `Total count: ${total_owned_magazines}/ ${total_magazin
 
 // Function to show only missing magazines
 const missing_magazines_btn = document.getElementById('missing_magazines_btn');
-missing_magazines_btn.addEventListener('click', () => {
-    covers_container.innerHTML = '';
-
-    const h2 = document.getElementById('year');
-    h2.innerHTML = "Missing Magazines";
-
-    inventory.forEach(magazine => {
-        if(magazine.OWNED == "NO") {
-            const div = document.createElement('div');
-            const img = document.createElement('img');
-            const p = document.createElement('p');
-            if(magazine['YEAR NUMBER'] == 'SPECIAL') {
-                img.src= `../imgs/${magazine['YEAR EDIT']}/SPECIAL.jpg`;
-                p.innerHTML = `${magazine.MONTH} - ${magazine['YEAR DATE']}`;
-            } else if (magazine['YEAR NUMBER'] == 'SPECIAL2'){
-                img.src= `../imgs/${magazine['YEAR EDIT']}/SPECIAL2.jpg`;
-                p.innerHTML = `${magazine.MONTH} - ${magazine['YEAR DATE']}`;
-            } else {
-                img.src= `../imgs/${magazine['YEAR EDIT']}/${magazine['YEAR NUMBER']}.jpg`
-                p.innerHTML = `${magazine.MONTH} - ${magazine['YEAR DATE']}`;
-            }
-            
-
-            // Not necessary
-            const p2 = document.createElement('p');
-            if(magazine.OWNED == 'YES') {
-                p2.innerHTML = 'Owned';
-            } else if(magazine.OWNED == 'YES-POOR') {
-                p2.innerHTML = 'Owned (poor)';
-            } else if(magazine.OWNED == 'NO') {
-                p2.innerHTML = 'Pending';
-            }
-            
-            p2.style.fontWeight = 'bold';
-
-            // Not necessary
-            if(p2.innerText == 'Owned' || p2.innerText == 'Owned (poor)') {
-                p2.style.color = '#00913f';
-            } else {
-                p2.style.color = '#FF0000';
-            }
-
-            const p3 = document.createElement('p');
-            p3.innerHTML = `# ${magazine['OVERALL NUMBER']}`;
-            p3.style.fontWeight = 'bold';
-
-            const p4 = document.createElement('p');
-            p4.innerHTML = `Year ${magazine['YEAR EDIT']} No. ${magazine['YEAR NUMBER']}`;
-            p4.style.fontWeight = 'bold';
-            
-            div.appendChild(img);
-            div.appendChild(p3);
-            div.appendChild(p4);
-            div.appendChild(p);
-            div.appendChild(p2);
-            covers_container.appendChild(div);
-        }
-    });
-    section2.style.display = 'block';
+missing_magazines_btn.addEventListener('click', (e) => {
+    e.preventDefault; // I had to add this line because the fucntion was called by default as soon as the page is loadad
+    let ext = 'jpg';
+    missingMagazines(inventory, ext);
 });
 
 // Function to show only poor condition magazines
